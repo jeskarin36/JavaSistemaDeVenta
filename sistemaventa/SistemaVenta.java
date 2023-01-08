edb2b07b379c83f3c25542572e0edbbfc33c06d0
jeskarin36 <94501234+jeskarin36@users.noreply.github.com>
2023-01-08 14:44:59 -0400

    Add files via upload

---
+++ sistemaventa/SistemaVenta.java
@@ -0,0 +1,14 @@
+package sistemaventa;
+
+import Vista.Login;
+
+
+public class SistemaVenta {
+
+   
+    public static void main(String[] args) {
+      Login lg=new Login();
+      lg.setVisible(true);
+    }
+    
+}
